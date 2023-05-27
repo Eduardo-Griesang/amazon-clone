a8d7214dcacccde47bca798b3178621ed8946448
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-05-27 15:37:02 -0300

    Header

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import './Navigation.css'
+
+const Navigation = () => {
+    return (
+        <section>
+            <img src='https://m.media-amazon.com/images/G/01/digital/video/web/Logo-min.png' alt='Amazom Prime Video Logo' />
+        </section>
+    )
+}
+
+export default Navigation
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,13 @@
+import './HomePage.css'
+
+import Navigation from '../../components/Navigation'
+
+const HomePage = () => {
+    return(
+        <header>
+            <Navigation />
+        </header>
+    )
+}
+
+export default HomePage
