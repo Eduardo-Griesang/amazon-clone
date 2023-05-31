fb0fdc120dcafdbb68d441189da4b37c6fdc54a3
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-05-30 22:22:01 -0300

    movies section

---
+++ src/components/Sections/index.js
@@ -0,0 +1,15 @@
+import "./Sections.css"
+
+const Sections = () => {
+    return(
+        <section className="sec">
+            <div className="sec-title">
+                <h2>Prime</h2> 
+                <span>Action Movies</span> 
+            </div>
+
+        </section>
+    )
+}
+
+export default Sections
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -2,6 +2,7 @@ import './HomePage.css'
 
 import Navigation from '../../components/Navigation'
 import Carousel from '../../components/Carousel'
+import Sections from '../../components/Sections'
 
 const HomePage = () => {
     return(
@@ -12,7 +13,7 @@ const HomePage = () => {
             </header>
 
             <main>
-                
+                <Sections />
             </main>
         </html>
     )
