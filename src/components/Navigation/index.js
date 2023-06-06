a8a297eb938ca11ec366a29506629f9943e7047b
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-06-05 22:42:22 -0300

    navegation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -11,7 +11,13 @@ const Navigation = () => {
                 <img src='https://m.media-amazon.com/images/G/01/digital/video/web/Logo-min.png' alt='Amazom Prime Video Logo' />
     
                 <li>
-                    <span className='active'>Home</span>
+                        <span className='active home'>Home</span>
+                        <ul className='homeDropdown'>
+                            <li>All</li>
+                            <li>Movies</li>
+                            <li>TV shows</li>
+                            <li>Sports</li>
+                        </ul>
                 </li>
                 <li>
                     <span>Store</span>
