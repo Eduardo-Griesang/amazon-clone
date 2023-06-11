7d075ed5ab748d614eb7cbfad799607ec139e9a6
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-06-11 16:53:46 -0300

    carousel

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -5,6 +5,35 @@ import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons
 
 const Sections = ({ API }) => {
 
+    const gap = 16;
+
+    const carousel = document.querySelector(".sec-title")
+    const content = document.querySelector(".sec-movies")
+    
+    function next (e) {
+        const next = e.target.id
+        carousel.scrollBy(width + gap, 0);
+        if (carousel.scrollWidth !== 0) {
+            prev.style.display = "flex";
+         }
+        if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+            next.style.display = "none";
+        }
+    }
+
+    function prev () {
+        carousel.scrollBy(-(width + gap), 0);
+        if (carousel.scrollLeft - width - gap <= 0) {
+            prev.style.display = "none";
+        }
+        if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+            next.style.display = "flex";
+        }
+    }
+
+    let width = carousel.offsetWidth;
+    window.addEventListener("resize", () => (width = carousel.offsetWidth));
+
     /* pega toda a lista de filmes e seleciona apenas os 10 primeiros, para ele não mostrar a lista completa */
     const filtered = API.slice(1, 10);
     
@@ -13,7 +42,7 @@ const Sections = ({ API }) => {
             <div className="sec-title">
 
                 <section>
-                    <h2>Prime</h2> 
+                    <h2 class="ola">Prime</h2> 
                     <span>Action Movies</span> 
                 </section>
 
@@ -27,8 +56,8 @@ const Sections = ({ API }) => {
                     })}
                 </section>
 
-                <FontAwesomeIcon className="next-btn btn" icon={faChevronRight} style={{color: "#ffffff",}} />
-                <FontAwesomeIcon className="prev-btn btn" icon={faChevronLeft} style={{color: "#ffffff",}} />
+                <FontAwesomeIcon className="next-btn btn" onClick={next} id="next" icon={faChevronRight} style={{color: "#ffffff",}} />
+                <FontAwesomeIcon className="prev-btn btn" onClick={prev} id="prev" icon={faChevronLeft} style={{color: "#ffffff",}} />
             </div>
         </section>
     )
