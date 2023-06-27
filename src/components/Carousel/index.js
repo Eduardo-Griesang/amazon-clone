efff5ae0d1f25bb931406a90fa7ce0b8afca11f9
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-06-27 15:16:35 -0300

    carousel fixed

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -11,10 +11,9 @@ const Carousel = () => {
                 <button type="button" data-bs-target="#carouselExampleCaptions" class="indicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div class="carousel-inner">
-                <div class="carousel-item active">
+                <div class="carousel-item active movie-info">
                     <img class='d-block movie-banner w-100' src="https://m.media-amazon.com/images/S/sonata-images-prod/SVOD_LATCANZ_Air_Launch_Concept_CS/69fec07f-e6ad-4de4-9955-a445c62f50fc._UR3840,1440_SX2160_FMwebp_.jpeg" alt="..."/>
-                    <div class="carousel-caption d-none d-md-block movie-info">
-
+                    <div class="datails-movie">
                         <img className='amazom-original' src='https://m.media-amazon.com/images/S/pv-target-images/7e5ebe135ef7e9f912cf8c29b6c9f4e22cab8ff78c0584a45c129cc1df33336e._AC_SX500_FMwebp_.png' alt='...' />
                         <img className='movie-title' src='https://m.media-amazon.com/images/S/sonata-images-prod/SVOD_LATCANZ_Air_Launch_Concept_CS/b247eb5a-f4a9-4deb-858b-fd5c5e92ce5d._AC_SX500_FMwebp_.png' alt='...' />
                         
