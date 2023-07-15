94785b95f5571f1c1657ecb2b678ce5675ac888a
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-07-14 23:29:39 -0300

    added react router funcionalities

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/HomePage';
+import MovieClicked from './components/MovieClicked'
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/MovieClicked' element={<MovieClicked />}/>
+        <Route path='*' element={<h1>404 NOT FOUND</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/MovieClicked/index.js
@@ -0,0 +1,9 @@
+import './MovieClicked.css'
+
+const MovieClicked = () => {
+    return (
+        <h1>clicou</h1>
+    )
+}
+
+export default MovieClicked
