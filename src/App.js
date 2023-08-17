b2264b425f64ffa33f28e172b0563f832e9e943e
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-08-17 10:37:28 -0300

    Scroll To Top

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import HomePage from './pages/HomePage';
 import MovieClicked from './pages/MovieClicked'
 import Navigation from './components/Navigation';
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navigation />
 
       <Routes>
